0bfc35ceaff4422f2bdfe9e6cfe45bd41c674d3a
Utente <Utente@DESKTOP-BAB4AAS>
2023-11-29 20:18:24 +0100

    Aggiungere i file di progetto.

---
+++ esercizio 24.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using esercizio_24._1.Properties;
+
+namespace esercizio_24._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrato q=new Quadrato();
+
+            Console.WriteLine("inserire lato");
+            q.Lato=float.Parse(Console.ReadLine());
+
+            Console.WriteLine("superficie quadrato: {0}", q.Lato * q.Lato);
+
+            Console.ReadLine();
+        }
+    }
+}
